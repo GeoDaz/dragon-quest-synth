405592ed2ec4a4dc59928da24aa8ecd458f0fcce
GeoDaz <gfrydz2@gmail.com>
2024-08-07 22:38:35 +0200

    Hot fix community

--- src/pages/community.tsx
+++ src/pages/community.tsx
@@ -1,17 +1,15 @@
 import { GetStaticProps } from 'next';
 import { Families, Monster as MonsterInterface } from '@/types/Monster';
 import PageLines from '.';
-import { makeMonsters, reverseSynth } from '@/functions/transformer/synthesis';
+import { reverseSynth } from '@/functions/transformer/synthesis';
 
 export const getStaticProps: GetStaticProps = async () => {
 	try {
 		const families: Families = require('../json/Families.json');
-
-		const monsters = makeMonsters(families);
-		reverseSynth(monsters);
+		reverseSynth(families);
 
 		const images = require('../json/monstersImages.json');
-		return { props: { monsters, families, images, dqm: false } };
+		return { props: { families, images, dqm: false } };
 	} catch (e) {
 		console.error(e);
 		return { props: {} };
